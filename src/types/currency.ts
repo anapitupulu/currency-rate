1ba11272e136e9305d0d319bf861ed78fdb826e3
Alex Napitupulu <sleigner@Alexs-MacBook-Pro.local>
2019-09-21 23:20:58 -0400

    docs(currency-rate): adds interface doc

--- src/types/currency.ts
+++ src/types/currency.ts
@@ -1,4 +1,14 @@
+/**
+ * Interface to model a currency data
+ */
 export interface ICurrency {
+
+  /** The label for the currency. E,g.: "Indonesian Rupiah" */
   readonly label: string;
+
+  /**
+   * The conversion rate from the base currency to this currency.
+   * It'll be populated by the data from exchangerateapi.io
+   */
   readonly rateFromBase?: number;
 }
